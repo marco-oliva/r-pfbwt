094ab33eff4281065a2e023be445e569a38f7171
Marco Oliva <marco.oliva@ufl.edu>
2022-10-26 09:55:08 -0400

    Working on easy suffixes

--- rpfbwt.cpp
+++ rpfbwt.cpp
@@ -2,6 +2,7 @@
 // Created by marco on 10/21/22.
 //
 
+#include <rle/rle_string.hpp>
 #include <pfp/pfp.hpp>
 
 #include <CLI/CLI.hpp>
@@ -26,16 +27,20 @@ int main(int argc, char **argv)
 
     // get L1 dictionary, occ and M1
     pfpds::dictionary<uint8_t> l1_d(l1_prefix, w1);
-    
+
     size_t d1_words; uint_t * occ;
     std::vector<uint_t> l1_freq( 1, 0);
     pfpds::read_file<uint_t> (std::string(l1_prefix + ".occ").c_str(), occ, d1_words); // here occ has the same size as the integers used for gsacak.
     l1_freq.insert(l1_freq.end(),occ, occ + d1_words);
-    
+
     pfpds::pf_parsing<uint8_t>::M_table l1_m(l1_d, l1_freq);
-    
+
     // get L2 PFP
-    std::string l2_prefix  = l1_prefix + ".parse";
+    std::string l2_prefix = l1_prefix + ".parse";
     pfpds::pf_parsing<uint32_t> l2_pfp(l2_prefix, w2);
+
+    // Iterate on M1 and output easy suffixes to a run length encoded string
+    rle::RLEString::RLEncoder easy_suffixes_encoder(l1_prefix + ".easy.rle");
+
 }
 
