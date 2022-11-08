89cb1cc6ca24a982ce3140ec3ae24e44a00f6381
Marco Oliva <marco.oliva@ufl.edu>
2022-11-07 19:04:03 -0500

    Update rpfbwt.cpp
    Add option for chunks per thread.

--- rpfbwt.cpp
+++ rpfbwt.cpp
@@ -16,12 +16,13 @@ int main(int argc, char **argv)
     std::size_t w2;
     std::size_t threads = 1;
     std::string tmp_dir;
-    std::size_t chunk_size = 0;
+    std::size_t chunks_per_thread = 5;
 
     app.add_option("--l1-prefix", l1_prefix, "Level 1 Prefix.")->configurable()->required();
     app.add_option("--w1", w1, "Level 1 window length.")->configurable()->required();
     app.add_option("--w2", w2, "Level 2 window length.")->configurable()->required();
     app.add_option("-t, --threads", threads, "Number of threads.")->configurable();
+    app.add_option("--chunks-per-thread", chunks_per_thread, "Number of chunks per thread.")->configurable();
     app.add_option("--tmp-dir", tmp_dir, "Temporary files directory.")->check(CLI::ExistingDirectory)->configurable();
     app.add_flag_callback("--version",rpfbwt::Version::print,"Version number.");
     app.set_config("--configure");
@@ -35,6 +36,6 @@ int main(int argc, char **argv)
     // Set threads accordingly to configuration
     omp_set_num_threads(threads);
     
-    rpfbwt::rpfbwt_algo<uint8_t> rpfbwt_algo(l1_prefix, w1, w2, threads * 5);
+    rpfbwt::rpfbwt_algo<uint8_t> rpfbwt_algo(l1_prefix, w1, w2, threads * chunks_per_thread);
     rpfbwt_algo.l1_bwt_parallel();
 }
